3be8809588e6f4558d624b705e039ae902e2c21c
Justin Menga <justin.menga@gmail.com>
2017-05-08 22:12:45 +1200

    Various simplifications and refactoring

--- src/ecs_tasks.py
+++ src/ecs_tasks.py
@@ -1,20 +1,23 @@
-import time
-import logging
 import sys, os
 parent_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(parent_dir, 'vendor')
 sys.path.append(vendor_dir)
 
+import time
+import logging
+import json
+from datetime import datetime
 from cfn_lambda_handler import Handler, CfnLambdaExecutionTimeout
 from hashlib import md5
 from lib import EcsTaskManager, EcsTaskFailureError, EcsTaskExitCodeError
-from lib import get_validator, validate
-from voluptuous import MultipleInvalid, Invalid
+from lib import validate
+from lib import error_handler
 
 # Set handler as the entry point for Lambda
 handler = Handler()
 
 # Configure logging
+logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
@@ -32,6 +35,10 @@ def start(task):
     started_by=task['StartedBy']
   )
 
+# Outputs JSON
+def format_json(data):
+  return json.dumps(data, default=lambda d: d.isoformat() if isinstance(d, datetime) else str(d))
+
 # Transforms a list of dicts into a keyed dictionary
 def to_dict(items, key, value):
   return dict(zip([i[key] for i in items], [i[value] for i in items]))
@@ -48,6 +55,12 @@ def get_task_definition_values(task_definition_arn, update_criteria):
   containers = to_dict(task_definition['containerDefinitions'],'name','environment')
   return [env['value'] for u in update_criteria for env in containers.get(u['Container'],{}) if env['name'] in u['EnvironmentKeys']]
 
+# Updates ECS task status
+def describe_tasks(cluster, task_result):
+  tasks = task_result['tasks']
+  task_arns = [t.get('taskArn') for t in tasks]
+  return task_mgr.describe_tasks(cluster=cluster, tasks=task_arns)
+
 # Checks ECS task completion
 def check_complete(task_result):
   if task_result.get('failures'):
@@ -63,57 +76,39 @@ def check_exit_codes(task_result):
     raise EcsTaskExitCodeError(tasks, non_zero)
 
 # Polls an ECS task for completion 
-def poll(task, event, context):
+def poll(task, remaining_time):
   poll_interval = task['PollInterval'] or 10
-  poll_count = task['Timeout'] / poll_interval + 1
-  counter = 0
-  log.info("Checking if task(s) have completed...")
-  while counter < poll_count:
-    if context.get_remaining_time_in_millis() < (poll_interval + 10) * 1000:
-      event['EventState'] = task
-      raise CfnLambdaExecutionTimeout()
-    complete = check_complete(task['TaskResult'])
-    if complete:
-      check_exit_codes(task['TaskResult'])
-      return task['TaskResult']
-    else:
+  task_result = task['TaskResult']
+  while True:
+    if remaining_time() < (poll_interval + 5) * 1000:
+      task['TaskResult'] = task_result
+      raise CfnLambdaExecutionTimeout(task)
+    if not check_complete(task_result):
       log.info("Task(s) have not yet completed, checking again in %s seconds..." % poll_interval)
       time.sleep(poll_interval)
-      tasks = task['TaskResult']['tasks']
-      task_arns = [t.get('taskArn') for t in tasks]
-      task['TaskResult'] = task_mgr.describe_tasks(cluster=task['Cluster'], tasks=task_arns)
-  raise Exception("The tasks did not complete in the specified timeout of %s seconds", task['Timeout'])
+      task_result = describe_tasks(task['Cluster'], task_result)
+    else:
+      check_exit_codes(task_result)
+      return task_result
 
-# Starts and ECS task and polls for the task result
-def start_and_poll(task, event, context):
+# Start and poll task
+def start_and_poll(task, context):
   task['TaskResult'] = start(task)
-  task['TaskResult'] = poll(task, event, context)
-  log.info("Task completed with result: %s" % task['TaskResult'])
+  task['TaskResult'] = poll(task,context.get_remaining_time_in_millis)
+  log.info("Task completed successfully with result: %s" % format_json(task['TaskResult']))
+  return next(t['taskArn'] for t in task['TaskResult']['tasks'])
 
-# Creates a task object from event data
-def create_task(event, context):
-  task = validate(event.get('ResourceProperties'))
-  event['Timeout'] = event.get('Timeout') or task['Timeout']
-  task['StartedBy'] = get_task_id(event.get('StackId'), event.get('LogicalResourceId'))
-  log.info('Received task %s' % str(task))
+# Create task
+def create_task(event):
+  task = validate(event['ResourceProperties'])
+  task['StartedBy'] = get_task_id(event['StackId'],event['LogicalResourceId'])
+  event['Timeout'] = task['Timeout']
+  log.info('Received task %s' % format_json(task))
   return task
-
-# Exception handler  
-def task_result_handler(func):
-  def handle_task_result(event, context):
-    try:
-      return func(event, context)
-    except EcsTaskFailureError as e:
-      return {"Status": "FAILED", "Reason": "A task failure occurred: %s" % e.failures}
-    except EcsTaskExitCodeError as e:
-      return {"Status": "FAILED", "Reason": "One or more containers failed with a non-zero exit code: %s" % e.non_zero}
-    except (Invalid, MultipleInvalid) as e:
-      return {"Status": "FAILED", "Reason": "One or more invalid properties: %s" % e}
-  return handle_task_result
 
 # Event handlers
 @handler.poll
-@task_result_handler
+@error_handler
 def handle_poll(event, context):
   log.info('Received poll event %s' % str(event))
   task = event.get('EventState')
@@ -122,38 +117,37 @@ def handle_poll(event, context):
   return {"Status": "SUCCESS", "PhysicalResourceId": task['StartedBy']}
 
 @handler.create
-@task_result_handler
+@error_handler
 def handle_create(event, context):
   log.info('Received create event %s' % str(event))
-  task = create_task(event, context)
+  task = create_task(event)
   if task['Count'] > 0:
-    start_and_poll(task, event, context)
-  return {"Status": "SUCCESS", "PhysicalResourceId": task['StartedBy']}
+    event['PhysicalResourceId'] = start_and_poll(task, context)
+  return event
 
 @handler.update
-@task_result_handler
+@error_handler
 def handle_update(event, context):
   log.info('Received update event %s' % str(event))
-  task = create_task(event, context)
+  task = create_task(event)
   update_criteria = task['UpdateCriteria']
   if task['RunOnUpdate'] and task['Count'] > 0:
     old_task = validate(event.get('OldResourceProperties'))
     if update_criteria:
-      old_values = get_task_definition_values(old_task['TaskDefinition'],task['UpdateCriteria'])
-      new_values = get_task_definition_values(task['TaskDefinition'],task['UpdateCriteria'])
+      old_values = get_task_definition_values(old_task['TaskDefinition'],update_criteria)
+      new_values = get_task_definition_values(task['TaskDefinition'],update_criteria)
       if old_values != new_values:
-        start_and_poll(task, event, context)
+        event['PhysicalResourceId'] = start_and_poll(task, context)
     else:
-      start_and_poll(task, event, context)
-  return {"Status": "SUCCESS", "PhysicalResourceId": task['StartedBy']}
+      event['PhysicalResourceId'] = start_and_poll(task, context)
+  return event
   
 @handler.delete
-@task_result_handler
+@error_handler
 def handle_delete(event, context):
   log.info('Received delete event %s' % str(event))
-  task = validate(event.get('ResourceProperties'))
-  task_id = get_task_id(event.get('StackId'), event.get('LogicalResourceId'))
-  tasks = task_mgr.list_tasks(cluster=task['Cluster'], startedBy=task_id)
+  task = create_task(event)
+  tasks = task_mgr.list_tasks(cluster=task['Cluster'], startedBy=task['StartedBy'])
   for t in tasks:
     service_mgr.stop_task(cluster=task['Cluster'], task=t, reason='Delete requested for %s' % event.get('StackId'))
-  return {"Status": "SUCCESS"}
+  return event
